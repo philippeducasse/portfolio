636e538060f1dff5d462745a5eb97fa7e2f91cab
Philippe Ducasse <ducassephi@hotmail.fr>
2026-04-19 10:50:02 +0200

    add AppShell: conditionally renders selector or portfolio

---
+++ src/components/AppShell.vue
@@ -0,0 +1,34 @@
+<template>
+  <ViewingModeSelector v-if="!currentTheme" />
+  <template v-else>
+    <NostalgicMarquee />
+    <Header @change-mode="clearTheme" />
+    <AnimationInit />
+    <main>
+      <Hero />
+      <Separator />
+      <Skills />
+      <Separator />
+      <Work />
+      <Separator />
+      <Projects />
+    </main>
+    <Footer />
+  </template>
+</template>
+
+<script setup lang="ts">
+import { useTheme } from "../utils/useTheme";
+import ViewingModeSelector from "./ViewingModeSelector.vue";
+import NostalgicMarquee from "./NostalgicMarquee.vue";
+import Header from "./Header.vue";
+import AnimationInit from "./AnimationInit.vue";
+import Hero from "./Hero.vue";
+import Separator from "./Separator.vue";
+import Skills from "./Skills.vue";
+import Work from "./Work.vue";
+import Projects from "./Projects.vue";
+import Footer from "./Footer.vue";
+
+const { currentTheme, clearTheme } = useTheme();
+</script>
